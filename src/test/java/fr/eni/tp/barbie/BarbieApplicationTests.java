03a18d3f32a632bba390f6671e0fbe55562235d9
Adrien LE CLECH <alc.theclech@gmail.com>
2026-01-13 12:13:36 +0100

    Init projet

---
+++ src/test/java/fr/eni/tp/barbie/BarbieApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.tp.barbie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
